4749bcb15a72537d0e8a29e03175a097228a1349
Maxime Baranowski <emouvaxy@gmail.com>
2019-10-16 14:04:10 +0200

    fix res.send.once

--- routers/menuRouter.js
+++ routers/menuRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connection = require('../db/database');
 
 router.get("/once", (req, res, next) => {
-    connection.query('CREATE TABLE entree(id_entree INT AUTO_INCREMENT PRIMARY KEY,nom VARCHAR(255),descr VARCHAR(255),prix INT);');
+    // connection.query('CREATE TABLE entree(id_entree INT AUTO_INCREMENT PRIMARY KEY,nom VARCHAR(255),descr VARCHAR(255),prix INT);');
     connection.query('CREATE TABLE plat(id_plat INT AUTO_INCREMENT PRIMARY KEY,nom VARCHAR(255),descr VARCHAR(255),prix INT);');
     connection.query('CREATE TABLE dessert(id_dessert INT AUTO_INCREMENT PRIMARY KEY,nom VARCHAR(255),descr VARCHAR(255),prix INT);');
     connection.query('CREATE TABLE menu(id_menu INT AUTO_INCREMENT PRIMARY KEY,id_entree INT,id_plat INT,id_dessert INT,FOREIGN KEY (id_entree) REFERENCES entree (id_entree),FOREIGN KEY (id_plat) REFERENCES plat (id_plat),FOREIGN KEY (id_dessert) REFERENCES dessert (id_dessert));');
