317c7b207ad26c87892991d3ea48f503f86d4792
Maxime Baranowski <emouvaxy@gmail.com>
2019-10-16 13:45:05 +0200

    fix sql connection

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const mysql = require ('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'mysql://b99a005133c9fb:242cb621@eu-cdbr-west-02.cleardb.net',
+    host: 'eu-cdbr-west-02.cleardb.net',
     user: 'b99a005133c9fb',
     password: '242cb621',
     database: 'heroku_d83f62009e365ad'
